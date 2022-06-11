2225e9b78b1fa41f9cbf633d056428a6e1e05d4e
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-12 01:18:01 +0300

    Added a function to return the last two drivers

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 const returnFirstTwoDrivers = (arr)=> {
     return `${arr[0]}  ${arr[1]}`;
 }
+
+const returnLastTwoDrivers = (arr)=> {
+    return  `${arr[arr.length - 2]}  ${arr[arr.length - 1]}`;
+}
